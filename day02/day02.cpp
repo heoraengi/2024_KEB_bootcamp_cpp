b27ca09e823dfc8b449f7527ee06c7863726422e
heoraengi <heo.raeng.i@gmail.com>
2024-01-24 14:32:53 +0900

    v0.1 day02

--- day02/day02.cpp
+++ day02/day02.cpp
@@ -20,24 +20,48 @@
 //}
 
 // 세자리 정수 입력하고 역순으로 출력시키기
+// #include <iostream>
+// using namespace std;
+// int main() {
+
+//     int num;
+//     int reverse_num = 0;
+//     int temp;
+
+//     cout << "세자리 정수를 입력하세요 : ";
+//     cin >> num;
+
+//     for (int i = 0; i < 3; i++) {
+//         temp = num % 10;
+//         reverse_num = (reverse_num * 10) + temp;
+//         num /= 10;
+//     }
+
+//     cout << "역순으로 출력 했습니다. : " << reverse_num << endl;
+
+//     return 0;
+// }
+
+//
+
+// 3-28
 #include <iostream>
+#include <iomanip>
 using namespace std;
-int main() {
 
-    int num;
-    int reverse_num = 0;
-    int temp;
+int main(){
+    double number;
+    int intPart;
+    double fractPart;
 
-    cout << "세자리 정수를 입력하세요 : ";
-    cin >> num;
+    cout << "소수를 입력하세요 : ";
+    cin >> number;
 
-    for (int i = 0; i < 3; i++) {
-        temp = num % 10;
-        reverse_num = (reverse_num * 10) + temp;
-        num /= 10;
-    }
-
-    cout << "역순으로 출력 했습니다. : " << reverse_num;
-
-    return 0;
+    intPart = static_cast<int>(number);
+    fractPart = number -intPart;
+    
+    cout << fixed << showpoint << setprecision(2);
+    cout <<"원래 입력한 소수 : " << number << endl;
+    cout <<"정수 부분 : " << intPart << endl;
+    cout <<"소수 부분 : " << fractPart << endl;
 }
