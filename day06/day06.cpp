230f06e448cb2d376e0c581b5f22c98226a2b295
heoraengi <heo.raeng.i@gmail.com>
2024-01-30 12:09:37 +0900

    v0.3 day06

--- day06/day06.cpp
+++ day06/day06.cpp
@@ -21,21 +21,38 @@
 //     // rn의 주소 : 0x7fffffffdb8c
 // }
 
+// #include <iostream>
+// using namespace std;
+// int addConstRef(const int& n1, const int& n2){
+//     return n1+ n2;
+// }
+
+// int addRef(int& n3, int& n4){
+//     return n3+n4;
+// }
+
+// int main(){
+//     cout << "상수참조를 사용 : "<< addConstRef(1,2) << endl;
+//     int n3 = 3;
+//     int n4 = 4;
+//     cout << "상수참조를 미사용 : "<< addRef(n3,n4) << endl;
+//     return 0;
+// }
+// // 상수참조를 사용하면 곧바로 함수 호출이 가능하다.
+// // 상수참조를 사용하지 않는다면 번거롭게 변수 할당을 한 후에 진행해야한다.
+
 #include <iostream>
 using namespace std;
-int addConstRef(const int& n1, const int& n2){
-    return n1+ n2;
-}
-
-int addRef(int& n3, int& n4){
-    return n3+n4;
-}
 
 int main(){
-    cout << "상수참조를 사용 : "<< addConstRef(1,2) << endl;
+    int arr[3] ={0};
 
-    int n3 = 3;
-    int n4 = 4;
-    cout << "상수참조를 미사용 : "<< addRef(n3,n4) << endl;
+    for(int i=0; i<(sizeof(arr)/sizeof(*arr)); i++){
+        cout<< "arr["<<i <<"] : " << &arr[i] << endl;
+        // 배열 주소가 4씩 증가함
+        // arr[0] : 0x7fffffffdb8c
+        // arr[1] : 0x7fffffffdb90
+        // arr[2] : 0x7fffffffdb94
+    }
     return 0;
 }
